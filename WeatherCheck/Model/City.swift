03a21258a85877f3479ac133082de806f34e4300
JusBug <pksgwo123@gmail.com>
2023-10-18 04:02:07 +0900

    feat: 검색 필터링 배열 City 타입 구현

---
+++ WeatherCheck/Model/City.swift
@@ -0,0 +1,35 @@
+//
+//  Cities.swift
+//  WeatherCheck
+//
+//  Created by Jusbug on 10/18/23.
+//
+
+struct City {
+    let location: String
+    let nation: String
+}
+
+let cities = [
+    City(location: "Seoul", nation: "South Korea"),
+    City(location: "Incheon", nation: "South Korea"),
+    City(location: "Busan", nation: "South Korea"),
+    City(location: "Tokyo", nation: "Japan"),
+    City(location: "Osaka", nation: "Japan"),
+    City(location: "Sapporo", nation: "Japan"),
+    City(location: "Kyoto", nation: "Japan"),
+    City(location: "Nagoya", nation: "Japan"),
+    City(location: "Paris", nation: "France"),
+    City(location: "London", nation: "United Kingdom"),
+    City(location: "Sydney", nation: "Australia"),
+    City(location: "Rome", nation: "Italy"),
+    City(location: "Moscow", nation: "Russia"),
+    City(location: "New York", nation: "USA"),
+    City(location: "Los Angeles", nation: "USA"),
+    City(location: "San Francisco", nation: "USA"),
+    City(location: "Athens", nation: "Greece"),
+    City(location: "Toronto", nation: "Canada"),
+    City(location: "Vancouver", nation: "Canada"),
+    City(location: "Prague", nation: "Czech Republic"),
+    City(location: "Berlin", nation: "Germany")
+]
