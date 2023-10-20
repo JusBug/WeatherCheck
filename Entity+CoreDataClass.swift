ea8ffe2fba8a8e4385e2a691e81d33acaab43383
JusBug <pksgwo123@gmail.com>
2023-10-20 11:41:20 +0900

    feat: CoreData 생성 및 CoreDataManager 구현

---
+++ Entity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Entity+CoreDataClass.swift
+//  WeatherCheck
+//
+//  Created by 박종화 on 10/20/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Entity)
+public class Entity: NSManagedObject {
+
+}
